b48b993f8f5bcb3c900fe1bead354a5fdbc171b5
ThainaTobias <alunathaina700@gmail.com>
2022-07-04 08:13:43 -0400

    criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./App.css"
+
+function App() {
+    const menu=[
+            
+    {   
+        link:"file:///C:/Users/USURIO~1/AppData/Local/Temp/Rar$DI00.383/big-king.webp",
+        nome:"Combo Big King",
+        desc:"Um hambúrger com duas carnes bovinas grelhadas de 57g, pão com gergelim, queijo...",
+        qtd:"Serve 1 pessoa",
+        price:"41,90"
+    },  
+    {
+        link:"",
+        nome:"Combo Cheddar Duplo",
+        desc:"Um hambúrger com duas carnes bovinas grelhadas de 57g, pão preto com gergelim,...",
+        qtd:"Serve 1 pessoa",
+        price:"41,90"
+        
+    },
+    {
+        link:"",
+        nome:"Combo Cheesebuerger Duplo com Bacon",
+        desc:"Um hambúrger com duas carnes bovinas grelhadas de 57g, pão com gergelim, queijo...",
+        qtd:"Serve 1 pessoa",
+        price:"36,90"
+        
+    },
+    {
+        link:"36,90",
+        nome:"Combo Mega Starck 2.0",
+        desc:"Um hambúrger com duas carnes bovinas grelhadas de 113g, pão com gergelim, queijo...",
+        qtd:"Serve 1 pessoa",
+        price:"R$ 51,90"
+        
+    }
+    ]
+    return (
+       <div>
+
+       </div>
+
+    );
+}
+
+export default App
